50b9420757980378856d94ea786a673b1030f9af
el2toro <vasea.badragan@gmail.com>
2025-03-17 21:06:17 +0100

    add dynamic time

--- src/app/features/home-feature/components/main-dashboard-time-section/main-dashboard-time-section.component.ts
+++ src/app/features/home-feature/components/main-dashboard-time-section/main-dashboard-time-section.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ToggleSwitchComponent } from "../../../../core/components/toggle-switch/toggle-switch.component";
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 interface Device{
   name: string;
@@ -12,22 +13,31 @@ interface Device{
   selector: 'app-main-dashboard-time-section',
   templateUrl: './main-dashboard-time-section.component.html',
   styleUrls: ['./main-dashboard-time-section.component.scss'],
-  imports: [ToggleSwitchComponent, CommonModule]
+  imports: [ToggleSwitchComponent, CommonModule, MatIconModule]
 })
 
 export class MainDashboardTimeSectionComponent implements OnInit {
   devices: Device[] = [
-    { name: 'Light', description: 'Bahlom Ltd', icon: 'assets/svg/light-bulb.svg' },
-    { name: 'Air Con', description: 'Sharp 320', icon: 'assets/svg/air-cond.svg' },
-    { name: 'Wifi', description: 'D-KM 345', icon: 'assets/svg/wifi.svg' }
-  ]
+    { name: 'Light', description: 'Bahlom Ltd', icon: 'light-bulb' },
+    { name: 'Air Con', description: 'Sharp 320', icon: 'air-cond' },
+    { name: 'Wifi', description: 'D-KM 345', icon: 'wifi' }
+  ];
+
+  date!: Date;
 
   constructor() { }
 
   ngOnInit() {
+    this. getTime();
   }
 
   switchOnOff(isOn: boolean) {
     console.log('is swiched on: ', isOn)
+  }
+
+  getTime(){
+    setInterval(() => {
+     this.date = new Date();
+    }, 1000)
   }
 }
